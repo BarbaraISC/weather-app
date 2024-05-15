f8bc4342c9f0bc654f4cb6d4911fed41fdebc141
BarbaraISC <barbaraisc93@gmail.com>
2024-05-15 10:55:20 +0100

    Added the search functionality using the api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "43b563a3f016fb0f8t1650905fb6o494";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#form-input");
-  let cityElement = document.querySelector("#city");
-
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Coimbra");
